58e289f3a18e9e72b72946b3bd2c84c79cfb7124
cuong77 <35688820+cuong77@users.noreply.github.com>
2019-10-13 21:28:20 +0700

    z

--- js/main.js
+++ js/main.js
@@ -314,3 +314,16 @@ $('#trigger-top-login-modal').click(function(){
 		$('#top-login-overlay, #top-login-modal').hide();
 	});
 
+
+
+    /*==================================================================
+    [ Show modal1 ]*/
+    $('.js-show-modal1').on('click',function(e){
+        e.preventDefault();
+        $('.js-modal1').addClass('show-modal1');
+    });
+
+    $('.js-hide-modal1').on('click',function(){
+        $('.js-modal1').removeClass('show-modal1');
+    });
+
